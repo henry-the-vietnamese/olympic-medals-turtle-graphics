a9a1bdad1bc348d26b61792bd15f0f826e819bf7
henry-the-vietnamese <henryfromvietnam@gmail.com>
2021-10-02 10:32:14 +0700

    add main playground for turtle

---
+++ main.py
@@ -0,0 +1,9 @@
+#
+# File:         main.py
+# Author:       Tan Duc Mai
+# Email:        tan.duc.work@gmail.com
+# Date:         5/8/2021
+# Description:  Introduce the user to the game, asking them to choose level.
+#   I hereby declare that I completed this work without any improper help
+#   from a third party and without using any aids other than those cited.
+#
